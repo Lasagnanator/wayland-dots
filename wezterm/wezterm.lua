55c729bca272ef8ed834b95e6116f9ad84786d95
Lasagnanator <d96a07@gmail.com>
2023-11-18 12:41:23 +0100

    Removed transparency and shrinked borders on Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,10 +13,16 @@ end
 
 -- For example, changing the color scheme:
 config.color_scheme = 'tokyonight_night'
--- Possible colorschemes: AdventureTime, Andromeda, tokyonight_variant, Tokyo Night [Variant] (Gogh)
+-- Possible colorschemes: AdventureTime, Andromeda, tokyonight_variant, Tokyo Night [Variant] (Gogh), SynthwaveAlpha
 -- config.default_prog = { 'bash' } -- remove for other login shell
-config.window_background_opacity = 0.85
+-- config.window_background_opacity = 0.85
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 4,
+  right = 4,
+  top = 4,
+  bottom = 4,
+}
 config.font = wezterm.font_with_fallback { { family = 'JetBrains Mono',  weight = 'Regular' }, { family = 'Symbols Nerd Font',  weight = 'Regular' } }
 config.font_size = 11.0
 -- config.cell_width = 0.8
