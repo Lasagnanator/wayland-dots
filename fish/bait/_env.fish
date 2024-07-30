ca516ab500bb26769361785e958cdeab4649b8a9
Lasagnanator <d96a07@gmail.com>
2024-07-30 15:17:59 +0200

    Added kubeconfig auto discovery

--- fish/bait/_env.fish
+++ fish/bait/_env.fish
@@ -6,3 +6,12 @@ set -gx QT_QPA_PLATFORMTHEME 'gtk2'
 set -gx EDITOR 'nvim'
 set -gx VISUAL 'nvim' 
 set -gx PATH $PATH ~/.local/bin ~/.scripts
+
+function get-kubeconfig
+    set -l KUBE_FILES ''
+    for FILE in $(find "$HOME/.kube/conf.d" -type f)
+        set KUBE_FILES "$KUBE_FILES$FILE:"
+    end
+    echo $(string trim --right --chars=':' $KUBE_FILES)
+end
+set -gx KUBECONFIG $(get-kubeconfig)
