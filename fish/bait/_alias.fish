c959d7544806698fe14bba834870b43388b3a30f
Lasagnanator <d96a07@gmail.com>
2024-07-30 15:13:26 +0200

    Added more default aliases

--- fish/bait/_alias.fish
+++ fish/bait/_alias.fish
@@ -12,8 +12,15 @@ alias vim 'nvim'
 alias sv 'sudo -E nvim'
 alias g 'git'
 alias r 'ranger-cd'
+alias d 'docker'
+alias k 'kubectl'
+alias t 'task'
+alias tf 'terraform'
 alias lz 'lazygit'
+alias ldk 'lazydocker'
 alias bt 'bluetoothctl'
+alias fman "man -k . | awk '{print \$1}' | fzf --header 'Select a command to open in man' --preview 'man {}' | xargs man"
+alias kdesc "kubectl get pods -A --no-headers | fzf | awk '{print \$2, \$1}' | xargs -n 2 sh -c 'kubectl describe pod \$0 -n \$1' | less --mouse"
 
 # Packet managers
 alias pac 'sudo pacman'
